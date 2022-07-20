fd182cc92dd29ea4eee54c132a583ac8004a8ba9
unknown <justsivan@gmail.com>
2022-07-20 18:04:13 +0300

    added app in main

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+const app = createApp(App)
+
+app.use(router)
+
+
 app.use(store)
 app.use(ElementPlus)
 app.mount('#app')
