842b71041c503891f1738c9b173ad57d1a56d122
unknown <justsivan@gmail.com>
2022-07-20 18:35:06 +0300

    commit to pull

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
+import './styles/styles.scss'
+
 import 'element-plus/dist/index.css'
+
 
 const app = createApp(App)
 
