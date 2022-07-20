3d66c14333d28adeb5386931dcccd0bf5a34698a
Amit Pariente <pukamit8886@gmail.com>
2022-07-20 18:24:37 +0300

    accept pulling changes

--- src/components/stay-preview.cmp.vue
+++ src/components/stay-preview.cmp.vue
@@ -1,21 +1,10 @@
 <template>
-<<<<<<< HEAD
-<el-card>
-  <div>
-    <img :src="stay.imgUrls[0]">
-    <span>{{stay.name}}</span>
-    <div class="buttom">{{stay.address.street}}</div>
-  </div>
-</el-card>
-=======
   <el-card>
     <div>
-      <img :src="stay.img" alt="">
       <span>{{ stay.name }}</span>
       <div class="buttom">{{ stay.loc.address }}</div>
     </div>
   </el-card>
->>>>>>> bdf822e03d5833a3608ffb71c87de3c61e9aedb9
 </template>
  <script>
 export default {
