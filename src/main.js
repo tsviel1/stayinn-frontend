016fa5ae83981ef12a53f1c4b4e8f9b969298333
Amit Pariente <pukamit8886@gmail.com>
2022-07-20 18:06:18 +0300

    accepting changes from pulling

--- src/main.js
+++ src/main.js
@@ -5,11 +5,7 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-<<<<<<< HEAD
-import "./styles/styles.scss"
-=======
 import './styles/styles.scss'
->>>>>>> bdf822e03d5833a3608ffb71c87de3c61e9aedb9
 
 const app = createApp(App)
 
