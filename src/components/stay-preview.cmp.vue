8ee1d8a60092fff0b1f78fbcdcd2879690b185af
unknown <justsivan@gmail.com>
2022-07-20 18:02:12 +0300

    accepted incoming changes in stay-preview

--- src/components/stay-preview.cmp.vue
+++ src/components/stay-preview.cmp.vue
@@ -1,13 +1,4 @@
 <template>
-<<<<<<< HEAD
-<el-card>
-  <div>
-    <img :src="stay.imgUrls[0]">
-    <span>{{stay.name}}</span>
-    <div class="buttom">{{stay.address.street}}</div>
-  </div>
-</el-card>
-=======
   <el-card>
     <div>
       <img :src="stay.img" alt="">
@@ -15,7 +6,6 @@
       <div class="buttom">{{ stay.loc.address }}</div>
     </div>
   </el-card>
->>>>>>> bdf822e03d5833a3608ffb71c87de3c61e9aedb9
 </template>
  <script>
 export default {
