3f115a76e0e7a8d237416009aa1a75a00b73d2e0
unknown <justsivan@gmail.com>
2022-07-26 22:08:15 +0300

    requests to server work

--- src/services/stay-service.js
+++ src/services/stay-service.js
@@ -1,4 +1,5 @@
 import { storageService } from './async-storage.service.js'
+import { httpService } from './http.service'
 import { db } from './data/airbnb.js'
 import newData from './data/stays.json'
 const STAY_KEY = 'stayDB'
@@ -7,63 +8,84 @@ const data = newData
 // console.log(data)
 _createStays()
 
-async function query(filterBy) {
-  // console.log(filterBy)
-  let stays = await storageService.query(STAY_KEY)
-  // console.log(stays)
-  if (!filterBy) {
-    return stays
-  }
-  if (filterBy.price) {
-    console.log(filterBy)
-    stays = stays.filter(
-      (stay) =>
-        filterBy.price.min <= stay.price && stay.price <= filterBy.price.max
-    )
-  }
-  if (filterBy.txt && filterBy.beds && filterBy.bedrooms) {
-    stays = stays.filter((stay) => {
-      return (
-        regex.test(stay.address.city) &&
-        filterBy.beds <= stay.beds &&
-        filterBy.bedrooms <= stay.bedrooms
-      )
-    })
-  } else if (filterBy.txt && filterBy.beds) {
-    stays = stays.filter((stay) => {
-      return regex.test(stay.address.city) && filterBy.beds <= stay.beds
-    })
-  } else if (filterBy.txt && filterBy.bedrooms) {
-    stays = stays.filter((stay) => {
-      return regex.test(stay.address.city) && filterBy.bedrooms <= stay.bedrooms
-    })
-  } else if (filterBy.beds && filterBy.bedrooms) {
-    stays = stays.filter(
-      (stay) => filterBy.beds <= stay.beds && filterBy.bedrooms <= stay.bedrooms
-    )
-  } else if (filterBy.txt) {
-    const regex = new RegExp(filterBy.txt, 'i')
-    stays = stays.filter((stay) => regex.test(stay.address.city))
-    // console.log(stays)
-  } else if (filterBy.beds) {
-    stays = stays.filter((stay) => filterBy.beds <= stay.beds)
-  } else if (filterBy.bedrooms) {
-    stays = stays.filter((stay) => filterBy.bedrooms <= stay.bedrooms)
-  }
+const ENDPOINT = 'stay'
 
-  return stays
+
+// async function query(filterBy) {
+// console.log(filterBy)
+//   let stays = await storageService.query(STAY_KEY)
+//   // console.log(stays)
+//   if (!filterBy) {
+//     return stays
+//   }
+//   if (filterBy.price) {
+//     console.log(filterBy)
+//     stays = stays.filter(
+//       (stay) =>
+//         filterBy.price.min <= stay.price && stay.price <= filterBy.price.max
+//     )
+//   }
+//   if (filterBy.txt && filterBy.beds && filterBy.bedrooms) {
+//     stays = stays.filter((stay) => {
+//       return (
+//         regex.test(stay.address.city) &&
+//         filterBy.beds <= stay.beds &&
+//         filterBy.bedrooms <= stay.bedrooms
+//       )
+//     })
+//   } else if (filterBy.txt && filterBy.beds) {
+//     stays = stays.filter((stay) => {
+//       return regex.test(stay.address.city) && filterBy.beds <= stay.beds
+//     })
+//   } else if (filterBy.txt && filterBy.bedrooms) {
+//     stays = stays.filter((stay) => {
+//       return regex.test(stay.address.city) && filterBy.bedrooms <= stay.bedrooms
+//     })
+//   } else if (filterBy.beds && filterBy.bedrooms) {
+//     stays = stays.filter(
+//       (stay) => filterBy.beds <= stay.beds && filterBy.bedrooms <= stay.bedrooms
+//     )
+//   } else if (filterBy.txt) {
+//     const regex = new RegExp(filterBy.txt, 'i')
+//     stays = stays.filter((stay) => regex.test(stay.address.city))
+//     // console.log(stays)
+//   } else if (filterBy.beds) {
+//     stays = stays.filter((stay) => filterBy.beds <= stay.beds)
+//   } else if (filterBy.bedrooms) {
+//     stays = stays.filter((stay) => filterBy.bedrooms <= stay.bedrooms)
+//   }
+
+//   return stays
+// }
+// await httpService.get(`${ENDPOINT}/${stayId}`)
+
+async function query(filterBy = {}) {
+  console.log(filterBy)
+  return await httpService.get(ENDPOINT, filterBy)
 }
 
-function getById(stayId) {
-  return storageService.get(STAY_KEY, stayId)
-}
+// function getById(stayId) {
+//   return storageService.get(STAY_KEY, stayId)
+// }
 
-function save(stay) {
-  const savedStay = stay._id
-    ? storageService.put(STAY_KEY, stay)
-    : storageService.post(STAY_KEY, stay)
-  return savedStay
-}
+async function getById(id) {
+  return await httpService.get(`${ENDPOINT}/${id}`)
+  }
+
+// function save(stay) {
+//   const savedStay = stay._id
+//     ? storageService.put(STAY_KEY, stay)
+//     : storageService.post(STAY_KEY, stay)
+//   return savedStay
+// }
+
+// async function save(stay) {
+//   if (stay._id) {
+//     const stay = httpService.post('stay')
+//   } else {
+
+//   }
+// }
 
 function removeReview(stayId, reviewId) {
   return get(stayId).then((stay) => {
@@ -83,7 +105,6 @@ function addReview(stayId, review) {
 }
 
 async function _createStays() {
-
   // console.log(data)
   let stays = await storageService.query(STAY_KEY)
   // console.log('CREATE STAYS:', stays)
@@ -95,7 +116,7 @@ async function _createStays() {
 export const stayService = {
   query,
   getById,
-  save,
+  // save,
   removeReview,
   addReview,
 }
