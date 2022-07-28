952c77d50499cf45dc13facfa3292c18b8b679e6
Amit Pariente <pukamit8886@gmail.com>
2022-07-28 14:50:07 +0300

    changes in login-signup

--- src/views/login-signup-page.cmp.vue
+++ src/views/login-signup-page.cmp.vue
@@ -1,51 +1,60 @@
 <template>
-<section class="login-signup-container">
-  <form v-if="isUser" class="container home-page main-layout">
-    <div class="login-header">
-      <h2>Good to see you again</h2>
-    </div>
-    <div class="login-section">
-      <div>
-      <p>Your Username</p>
-      <input class="input" type="text" placeholder="e.g pukii886">
+  <section class="login-signup-container">
+    <form v-if="isUser" @submit.prevent="doLogin" class="container home-page main-layout">
+      <div class="login-header">
+        <h2>Good to see you again</h2>
       </div>
-      <div>
-        <p>Your password</p>
-        <input class="input" type="password" placeholder="eg iloveStayin123">
+      <div class="login-section">
+        <div>
+          <p>Your Username</p>
+          <input v-model="loginCred.username" class="input" type="text" placeholder="e.g pukii886">
+        </div>
+        <div>
+          <p>Your password</p>
+          <input v-model="loginCred.password" class="input" type="password" placeholder="eg iloveStayin123">
+        </div>
+        <div>
+          <button class="signup-btn">Sign in</button>
+        </div>
+        <a @click="isUser = !isUser">Dont have an account? sign up</a>
       </div>
-      <div>
-        <button class="signup-btn">Sign in</button>
+    </form>
+    <form v-else @submit.prevent="doSignup" class="container home-page main-layout">
+      <div class="signup-section">
+        <h2 class="signup-header">Welcome to Stayin</h2>
+        <input v-model="signupCred.fullname" class="input" type="text" placeholder="Full name">
+        <input v-model="signupCred.username" class="input" type="text" placeholder="Username">
+        <input v-model="signupCred.password" class="input" type="password" placeholder="Password">
+        <button class="signup-btn">Sign up</button>
       </div>
-      <a @click="isUser=!isUser">Dont have an account? sign up</a>
-    </div>
-  </form>
-  <form v-else class="container home-page main-layout">
-    <div class="signup-section">
-      <h2 class="signup-header">Welcome to Stayin</h2>
-      <input class="input" type="text" placeholder="Full name">
-      <input class="input" type="text" placeholder="Username">
-      <input class="input" type="password" placeholder="Password">
-      <input class="input" type="text" placeholder="Email">
-      <button class="signup-btn">Sign up</button>
-    </div>
-
-
-  </form>
-</section>
+    </form>
+  </section>
 </template>
  <script>
- export default {
- name: 'login-signup',
-    components: {},
+export default {
+  name: 'login-signup',
+  components: {},
   data() {
-   return {
-    isUser:true
-   };
+    return {
+      isUser: true,
+      loginCred: { username: 'user1', password: '123' },
+      signupCred: { username: '', password: '', fullname: '' }
+    };
+  },
+  methods: {
+    async doLogin() {
+      try {
+        await this.$store.dispatch({ type: "login", userCred: this.loginCred })
+        this.$router.push('/')
+      } catch (err) {
+        console.log(err)
+      }
     },
-  created() {},
- methods: {},
- computed: {},
- unmounted() {},
-  };
-  </script>
- <style></style>
+    async doSignup() {
+      await this.$store.dispatch({ type: 'signup', userCred: this.signupCred })
+      this.$router.push('/')
+
+    },
+  },
+}
+</script>
