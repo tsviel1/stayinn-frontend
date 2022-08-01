edd87eb0c89232a66fc915deb40d6af6baa2fadd
Amit Pariente <pukamit8886@gmail.com>
2022-08-01 14:40:05 +0300

    user msg css changes

--- src/components/user-msg.vue
+++ src/components/user-msg.vue
@@ -1,54 +1,86 @@
 <template>
   <div v-if="msg" class="user-msg" :class="msg.type">
     <div class="texts">
-      <h2>{{ msg.type }}</h2>
-      <p>{{ msg.txt }}</p>
-      <router-link v-if="msg.link" :to="msg.link" class="learn-more"
-        >Learn More about this website</router-link
-      >
+      <!-- <h2>{{ msg.type }}</h2> -->
+      <p>{{ msg.txt }} </p>
+     <close-svg @click="msg=!msg"/>
+      <router-link v-if="msg.link" :to="msg.link" class="learn-more">Learn More about this website</router-link>
     </div>
   </div>
 </template>
 
 <script>
-  import {eventBus} from '@/services/event-bus.service.js';
-  export default {
-    data() {
-      return {
-        msg: null,
-        removeEventListener: null,
-      };
+import closeSvg from './svg/close-svg.vue';
+import { eventBus } from '@/services/event-bus.service.js';
+import CloseSvg from './svg/close-svg.vue';
+
+
+export default {
+  data() {
+    return {
+      msg: null,
+      removeEventListener: null,
+    };
+  },
+  created() {
+    this.unsubscribe = eventBus.on('show-msg', this.showMsg);
+  },
+  components: {
+    closeSvg,
+    CloseSvg
+},
+  methods: {
+    showMsg(msg) {
+      this.msg = msg;
+      // setTimeout(() => {
+      //   this.msg = null;
+      // }, 3000);
     },
-    created() {
-      this.unsubscribe = eventBus.on('show-msg', this.showMsg);
-    },
-    methods: {
-      showMsg(msg) {
-        this.msg = msg;
-        setTimeout(() => {
-          this.msg = null;
-        }, 3000);
-      },
-    },
-    unmounted() {
-      this.unsubscribe();
-    },
-  };
+  },
+  unmounted() {
+    this.unsubscribe();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .user-msg {
-    position: absolute;
-    top: 16px;
-    right: 16px;
-    background-color: rgb(122, 209, 122);
-    padding: 10px 20px;
-    &.success {
-      background-color: lightgreen;
-    }
-    &.error {
-      background-color: red;
-      color: white;
-    }
+.user-msg {
+  position: absolute;
+  top: 8%;
+  right: 20.5%;
+  // left: 50%;
+  padding: 15px 15px 15px 20px;
+
+  border-radius: 5px;
+
+  &.success {
+    background-color: #f0f9eb;
+    border: 1px solid #e1f3d8;
+    color: #67c23a;
   }
+
+  &.error {
+    background-color: #fef0f0;
+    border: 1px solid #fde2e2;
+    color: #f56c6c;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  .texts {
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    svg {
+      height: 20px;
+      padding-left: 10px;
+      text-align: center;
+      color: #a8abb2;
+    }
+
+  }
+}
 </style>
